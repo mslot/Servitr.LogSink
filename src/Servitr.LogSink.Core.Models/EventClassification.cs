e9c6b3b2d9373ce1dc21877ee15721f26611a03d
Martin Slot <mas@impact.dk>
2020-02-03 10:40:56 +0100

    FIxed merge

--- src/Servitr.LogSink.Core.Models/EventClassification.cs
+++ src/Servitr.LogSink.Core.Models/EventClassification.cs
@@ -7,5 +7,7 @@ namespace Servitr.LogSink.Core.Models
     {
         public int EventId { get; set; }
         public string Name { get; set; }
+        public string AreaName { get; set; }
+
     }
 }
