136b7acd559a3ead3bd1064eb1151c8b74020a60
Martin Slot <mas@impact.dk>
2020-04-04 19:55:35 +0200

    Removed unused code

--- src/Servitr.LogSink.TestConsole/Program.cs
+++ src/Servitr.LogSink.TestConsole/Program.cs
@@ -1,10 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Moq;
 using Servitr.LogSink.EventIdMappers.InMemoryEventIdMapper;
 using Servitr.LogSink.Interfaces;
-using System;
 using System.Threading.Tasks;
 
 namespace Servitr.LogSink.TestConsole
@@ -17,31 +14,6 @@ namespace Servitr.LogSink.TestConsole
             await CreateHostBuilder(args)
                                         .Build().
                                         RunAsync();
-            //    var envMock = new Mock<IHostEnvironment>();
-            //    envMock.Setup(env => env.EnvironmentName).Returns("TestConsole");
-
-            //    //I want a real factory
-            //    var serviceProvider = new ServiceCollection()
-            //                                .AddLogging((config) =>
-            //                                {
-            //                                    config.AddConsole();
-            //                                    config.AddDebug();
-            //                                })
-
-            //                                .BuildServiceProvider();
-
-            //    var factory = serviceProvider.GetService<ILoggerFactory>();
-
-            //    var eventIdMapper = new InMemoryEventIdMapper();
-
-            //    _sink = new SimpleLogSink(
-            //        envMock.Object,
-            //        factory,
-            //        eventIdMapper);
-            //    //var l = factory.CreateLogger<Program>();
-            //    //l.LogInformation("hej");
-            //    _sink.LogInformation<Program>("Log message {param1}{param2}", 0, "this is parameter 1", "this is parameter 2");
-            //}
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
