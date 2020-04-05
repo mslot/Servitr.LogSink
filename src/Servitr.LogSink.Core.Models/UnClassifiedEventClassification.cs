37353e5b2f96dac1d257b68c40da0723059580a8
Martin Slot <mas@impact.dk>
2020-04-05 20:07:59 +0200

    Added auto property

--- src/Servitr.LogSink.Core.Models/UnClassifiedEventClassification.cs
+++ src/Servitr.LogSink.Core.Models/UnClassifiedEventClassification.cs
@@ -4,9 +4,8 @@ namespace Servitr.LogSink.Core.Models
 {
     public class UnClassifiedEventClassification : IEventClassification
     {
-        private string _name = "UnClassifiedEvent";
         public int EventId { get => 0; set => _ = 0; }
-        public string Name { get => _name; set => _name=value; }
+        public string Name { get; set; } = "UnClassifiedEvent";
         public string AreaName { get; set; }
     }
 }
